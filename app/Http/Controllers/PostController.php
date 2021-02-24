b4627851ed62b94df0a13493ddf1ddc25ecd2c9f
Javier Reyes <javier.reys@gmail.com>
2021-02-24 18:20:18 -0300

    feat: functional API with route resources.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // Get all posts
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Create a post
+        return Post::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // Show a post
+        return Post::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // Update a post
+        $post = Post::find($id);
+        $post->update($request->all());
+
+        return $post;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // Delete a post
+        return Post::destroy($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,33 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// posts
+// CRUD is basically
+
+// 1. get all (GET) /api/posts
+// 2. create a post (POST) /api/posts
+// 3. get a single (GET) /api/posts/{id}
+// 4. update a single (PUT/PATCH) /api/posts/{id}
+// 5. delete (DELETE) /api/posts/{id}
+
+// The following lines of code is to do the routes
+// by hand.
+// Route::get('posts', [PostController::class, 'index']);
+// Route::post('posts', 'PostController@store');
+// Route::put('posts', 'PostController@update');
+// Route::delete('posts', 'PostController@destroy');
+
+// Now the short-hand to do it:
+
+Route::resource('posts', PostController::class);
+
+// to create a resource (posts) in laravel
+//  1. create the database and migrations
+//  2. create a model to connect with the database
+//  2.5. create a service? Eloquent ORM
+//  3. create a controller to go get info from the database
+//  4. return that info
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
